869d430c4dc4d8c52ce97bffa1539bce31c8c382
Priyansh-Bharti <priyanshbharti102@gmail.com>
2026-06-21 18:49:24 +0530

    chore: remove junk files from repo, update .gitignore

--- public/update_links.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const files = ['index.html', 'forest.html', 'dashboard.html', 'coach.html', 'card.html', 'actions.html'];
-for (const file of files) {
-  let content = fs.readFileSync(file, 'utf8');
-  content = content.replace(/style="([^"]*text-decoration: none;)"(\s+id="nav-link-(?:dashboard|actions|coach|share)")/g, 'style="$1 display: none;"$2');
-  fs.writeFileSync(file, content, 'utf8');
-  console.log('Updated ' + file);
-}
